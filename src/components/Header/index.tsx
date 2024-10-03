d470873ac1642535dc439d1a630f19aa3d43f251
FelipeTomazetti <felipe.pistori@gmail.com>
2024-10-03 20:28:18 -0300

    [FT] - feat: cria arquivo de header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="py-4 bg-blue-600 text-white text-center">
+      <h1 className="text-4xl font-bold">Felipe Tomazetti</h1>
+      <p className="text-lg">
+        A passionate developer, change the world bit by bit.
+      </p>
+    </header>
+  );
+};
+
+export default Header;
