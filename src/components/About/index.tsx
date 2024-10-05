a0ed1848f1f642f3b589b1d5f5c5113a21adf708
FelipeTomazetti <felipe.pistori@gmail.com>
2024-10-04 23:32:15 -0300

    [FT] - chore: melhora texto de sobre

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,7 +5,10 @@ const About = () => {
       <p className="text-lg">
         Me chamo Felipe, sou de Curitiba, tenho 34 anos, formado em Engenharia
         Mecânica pela UTFPR. Sou casado, pai da Zoe (minha cachorrinha viralata
-        de 2 anos).
+        de 2 anos). Inicie minha carreira trabalhando com Customer Success e
+        Suporte me encontrei na programação, onde estou há 2 anos trabalhando
+        como Front End, meu objetivo é me tornar referência no que eu faço e
+        poder ajudar os outros através do conhecimento.
       </p>
     </div>
   );
